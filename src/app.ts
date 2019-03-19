ec84b13227dd680bd9662d83120fd4291818c986
ioanStefan <31771469+ioanStefan@users.noreply.github.com>
2019-03-19 20:15:02 +0200

    Version 1.0.0

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+import path from "path";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "handlebars");
+
+export default app;
